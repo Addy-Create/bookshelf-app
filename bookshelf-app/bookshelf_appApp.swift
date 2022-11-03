90a6f96859f4582a1c40398fdb8596f9593407cb
addy-create <addycreate.info@gmail.com>
2022-11-03 11:59:06 -0400

    Initial Commit

---
+++ bookshelf-app/bookshelf_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookshelf_appApp.swift
+//  bookshelf-app
+//
+//  Created by Aubrey Addy on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct bookshelf_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
